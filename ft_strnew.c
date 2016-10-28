48246501b8f5ad25a5d5f71edb08478c7485f578
Vincent Arnaud <varnaud@student.42.us.org>
2016-10-27 18:25:08 -0700

    allow creation of string with 0 length

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: varnaud <varnaud@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/28 19:27:41 by varnaud           #+#    #+#             */
-/*   Updated: 2016/09/28 19:41:01 by varnaud          ###   ########.fr       */
+/*   Updated: 2016/10/27 18:24:23 by varnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@ char	*ft_strnew(size_t size)
 	char	*str;
 	char	*p;
 
-	str = (char*)malloc(size + sizeof(char));
+	str = (char*)malloc((size + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
 	p = str;
-	while (size--)
+	while (size)
+	{
 		*p++ = '\0';
+		size--;
+	}
 	*p = '\0';
 	return (str);
 }
