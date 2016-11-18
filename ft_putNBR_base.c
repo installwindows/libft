94dac0bbdbd8425ec83ae9e44aa984301a31cd90
Vincent Arnaud <varnaud@student.42.us.org>
2016-11-18 13:44:37 -0800

    derp

--- ft_putNBR_base.c
+++ ft_putNBR_base.c
@@ -6,7 +6,7 @@
 /*   By: varnaud <varnaud@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/30 17:14:15 by varnaud           #+#    #+#             */
-/*   Updated: 2016/11/18 12:55:19 by varnaud          ###   ########.fr       */
+/*   Updated: 2016/11/18 13:43:42 by varnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ void	ft_putNBR_base(long long nbr, int base)
 		ft_putchar('-');
 		if (nbr <= -base)
 			ft_putNBR_base(nbr / -base, base);
-		ft_putchar(ft_itoc(-(nbr % base)));
+		ft_putchar(ft_itoC(-(nbr % base)));
 	}
 	else if (nbr >= base)
 	{
 		ft_putNBR_base(nbr / base, base);
-		ft_putchar(ft_itoc(nbr % base));
+		ft_putchar(ft_itoC(nbr % base));
 	}
 	else
-		ft_putchar(ft_itoc(nbr % base));
+		ft_putchar(ft_itoC(nbr % base));
 }
