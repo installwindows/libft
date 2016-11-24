eb7197861089b3dca6a6369fd146d97fd1b605ab
Vincent Arnaud <varnaud@student.42.us.org>
2016-11-24 01:05:57 -0800

    add ft_count_words

---
+++ ft_count_words.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_count_words.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: varnaud <varnaud@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/24 01:04:31 by varnaud           #+#    #+#             */
+/*   Updated: 2016/11/24 01:05:04 by varnaud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		get_count_words(char const *s, char c)
+{
+	size_t	i;
+	int		nb_words;
+	int		new_word;
+
+	i = 0;
+	nb_words = 0;
+	new_word = 1;
+	while (s[i])
+	{
+		if (new_word && s[i] != c)
+		{
+			nb_words++;
+			new_word = 0;
+		}
+		if (s[i] == c)
+			new_word = 1;
+		i++;
+	}
+	return (nb_words);
+}
